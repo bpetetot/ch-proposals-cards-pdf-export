9811f8dab7123ce934b775e1b6017ea905c849c2
Benjamin Petetot <bpetetot.external@lafourchette.com>
2019-07-26 15:54:18 +0200

    :sparkles: generate pdf

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const { template, slice, pick, isNil } = require('lodash')
+const puppeteer = require('puppeteer')
 
 const cardTemplate = require('./card.template.js')
 const cardsTemplate = require('./cards.template.js')
@@ -32,19 +33,54 @@ const getCards = ({ speakers, formats, categories, talks }) => {
   })
 }
 
-const cardTemplateCompiled = template(cardTemplate)
+const printPDF = async () => {
+  console.log('Generate cards...')
+  const cardTemplateCompiled = template(cardTemplate)
+  const cards = getCards(data).map(card => cardTemplateCompiled({ card }))
 
-const cards = getCards(data).map(card => cardTemplateCompiled({ card }))
+  const PAGE_SIZE = 8
+  const pages = []
+  for (let i = 0; i < cards.length; i = i + PAGE_SIZE) {
+    const page = slice(cards, i, i + PAGE_SIZE).join('\n')
+    pages.push(page)
+  }
 
-const PAGE_SIZE = 8
-const pages = []
-for (let i = 0; i < cards.length; i = i + PAGE_SIZE) {
-  const page = slice(cards, i, i + PAGE_SIZE).join('\n')
-  pages.push(page)
+  const cardsTemplateCompiled = template(cardsTemplate)
+
+  const html = cardsTemplateCompiled({ pages })
+
+  console.log('Launch puppeteer...')
+  const browser = await puppeteer.launch({ headless: true });
+
+  console.log('Open cards page...')
+  const page = await browser.newPage();
+  await page.setContent(html);
+
+  console.log('Generate PDF cards...')
+  const pdf = await page.pdf({ path: 'cards.pdf',format: 'A4' });
+  console.log('PDF cards generated ✨')
+
+  await browser.close();
+  return pdf
 }
 
-const cardsTemplateCompiled = template(cardsTemplate)
+printPDF()
 
-const cardsHtml = cardsTemplateCompiled({ pages })
+// res.setHeader({ 'Content-Type': 'application/pdf', 'Content-Length': pdf.length })
+// res.send(pdf);
 
-console.log(cardsHtml)
+// savePDF = () => {
+//   this.openModal(‘Loading…’) // open modal
+//  return getPDF() // API call
+//    .then((response) => {
+//      const blob = new Blob([response.data], {type: 'application/pdf'})
+//      const link = document.createElement('a')
+//      link.href = window.URL.createObjectURL(blob)
+//      link.download = `your-file-name.pdf`
+//      link.click()
+//      this.closeModal() // close modal
+//    })
+//  .catch(err => /** error handling **/)
+// }
+
+// <button onClick={this.savePDF}>Save as PDF</button>
